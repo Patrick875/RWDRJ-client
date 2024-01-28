01bfe71c397f4179b9c59b98abcc3bf273df8e82
KUBWIMANA Patrick <52809972+Patrick875@users.noreply.github.com>
2024-01-28 13:20:43 +0200

    first demo deployment

--- src/Screens/Footer/index.tsx
+++ src/Screens/Footer/index.tsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import Logo from "../../assets/logo.png";
+import Logo from "../../assets/Logo.png";
 import { navs } from "../../constants";
 import SocialLinks from "../../Shared/Topbar/SocialLinks";
 const Footer = () => {
