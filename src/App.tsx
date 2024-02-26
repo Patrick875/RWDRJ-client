6e3933a18e81a83c1f158c26a3cf6070cef4d8f9
KUBWIMANA Patrick <52809972+Patrick875@users.noreply.github.com>
2024-02-26 16:21:07 +0200

    still trying to fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import { Route, Routes } from "react-router-dom";
 import Layout from "./Screens/Layout";
 import AboutUs from "./Screens/AboutUs";
-import LayoutAdmin from "./Screens/Admin/LayoutAdmin";
-import Login from "./Screens/Login";
-import RegisterUser from "./Screens/RegisterUser";
-import AdminDashboard from "./Screens/Admin/AdminDashboard";
-import PrivateRoute from "./Screens/Admin/PrivateRoute";
+// import LayoutAdmin from "./Screens/Admin/LayoutAdmin";
+// import Login from "./Screens/Login";
+// import RegisterUser from "./Screens/RegisterUser";
+// import AdminDashboard from "./Screens/Admin/AdminDashboard";
+// import PrivateRoute from "./Screens/Admin/PrivateRoute";
 import WhoWeAre from "./Screens/WhoWeAre";
 
 import OurMission from "./Screens/OurMission";
@@ -22,16 +22,16 @@ import News from "./Screens/News";
 import Blogs from "./Screens/Blogs";
 import Events from "./Screens/Events";
 
-import HomeDashboard from "./Screens/Admin/HomeDashboard";
-import Pages from "./Screens/Admin/Pages";
-import BlogPage from "./Screens/Admin/BlogPage";
-import EventsAdmin from "./Screens/Admin/EventsAdmin";
-import AllBlogs from "./Screens/Admin/Blogs/AllBlogs";
-import CreateBlog from "./Screens/Admin/Blogs/CreateBlog";
-import ViewBlog from "./Screens/Admin/Blogs/ViewBlog";
-import AllEvents from "./Screens/Admin/Events/AllEvents";
-import CreateEvent from "./Screens/Admin/Events/CreateEvent";
-import ViewEvent from "./Screens/Admin/Events/ViewEvent";
+// import HomeDashboard from "./Screens/Admin/HomeDashboard";
+// import Pages from "./Screens/Admin/Pages";
+// import BlogPage from "./Screens/Admin/BlogPage";
+// import EventsAdmin from "./Screens/Admin/EventsAdmin";
+// import AllBlogs from "./Screens/Admin/Blogs/AllBlogs";
+// import CreateBlog from "./Screens/Admin/Blogs/CreateBlog";
+// import ViewBlog from "./Screens/Admin/Blogs/ViewBlog";
+// import AllEvents from "./Screens/Admin/Events/AllEvents";
+// import CreateEvent from "./Screens/Admin/Events/CreateEvent";
+// import ViewEvent from "./Screens/Admin/Events/ViewEvent";
 import BlogDetails from "./Screens/Blogs/BlogDetails";
 import EventDetails from "./Screens/Events/EventDetails";
 import { Toaster } from "react-hot-toast";
@@ -76,27 +76,6 @@ function App() {
 						/>
 						<Route />
 					</Route>
-					<Route path="/admin" element={<LayoutAdmin />}>
-						<Route index element={<Login />} />
-						<Route path="register" element={<RegisterUser />} />
-						<Route
-							path="dashboard"
-							element={<PrivateRoute element={<AdminDashboard />} />}>
-							<Route index element={<HomeDashboard />} />
-							<Route path="pages" element={<Pages />} />
-							<Route path="blogs" element={<BlogPage />}>
-								<Route index element={<AllBlogs />} />
-								<Route path="create" element={<CreateBlog />} />
-								<Route path=":refId" element={<ViewBlog />} />
-							</Route>
-							<Route path="events" element={<EventsAdmin />}>
-								<Route index element={<AllEvents />} />
-								<Route path="create" element={<CreateEvent />} />
-								<Route path=":refId" element={<ViewEvent />} />
-							</Route>
-							<Route path="events" element={<EventsAdmin />} />
-						</Route>
-					</Route>
 				</Routes>
 			</AnimatePresence>
 		</>
@@ -104,3 +83,27 @@ function App() {
 }
 
 export default App;
+
+{
+	/* <Route path="/admin" element={<LayoutAdmin />}>
+	<Route index element={<Login />} />
+	<Route path="register" element={<RegisterUser />} />
+	<Route
+		path="dashboard"
+		element={<PrivateRoute element={<AdminDashboard />} />}>
+		<Route index element={<HomeDashboard />} />
+		<Route path="pages" element={<Pages />} />
+		<Route path="blogs" element={<BlogPage />}>
+			<Route index element={<AllBlogs />} />
+			<Route path="create" element={<CreateBlog />} />
+			<Route path=":refId" element={<ViewBlog />} />
+		</Route>
+		<Route path="events" element={<EventsAdmin />}>
+			<Route index element={<AllEvents />} />
+			<Route path="create" element={<CreateEvent />} />
+			<Route path=":refId" element={<ViewEvent />} />
+		</Route>
+		<Route path="events" element={<EventsAdmin />} />
+	</Route>
+</Route>; */
+}
